3bd84306f13ae3e2078f1b7c5ded763e58348b49
santi pascual <santi.pdp@gmail.com>
2014-05-06 02:36:09 +0100

    duration calculation bug corrected

--- wavio/main.cpp
+++ wavio/main.cpp
@@ -11,23 +11,23 @@
 
 int main(int argc, const char * argv[])
 {
-/*    if(argc<2)
+    if(argc<2)
         std::cerr << "Usage: " << argv[0] << " <input file>" << std::endl;
     WavFile wave(argv[1]);
     wave.printHeader();
     //print file duration in seconds
     WAV_header hdr = wave.getHeader();
-    int samples = hdr.data.subChunk2Size/(hdr.fmt.bitsPerSample/8);
+    int samples = hdr.data.subChunk2Size/((hdr.fmt.bitsPerSample/8)*hdr.fmt.numOfChan);
     double seconds = (double)samples/(double)hdr.fmt.samplingRate;
-    std::cerr << "file duration (seconds): " << seconds << std::endl;*/
-    WavFile in("/tmp/test.wav");
+    std::cerr << "file duration (seconds): " << seconds << std::endl;
+/*    WavFile in("/tmp/test.wav");
     in.printHeader();
     
     WavFile out;
     WAV_header header = in.getHeader();
     out.setHeader(&header);
     std::vector<short> data = in.getData();
-    out.writeWavFile("/tmp/outt.wav", data);
+    out.writeWavFile("/tmp/outt.wav", data);*/
     return 0;
 }
 
